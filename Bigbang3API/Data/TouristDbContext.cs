63e009509d361661688880e85a1c50a8f46ce30d
Shruthika14 <shruthika.kanini@gmail.com>
2023-07-31 18:43:47 +0530

    JWT for Implemented JWT-based authentication and role-based access control for secure user enrollment, login, and API authorization in Kanini Tourism website.

---
+++ Bigbang3API/Data/TouristDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Bigbang3API.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Bigbang3API.Data
+{
+    public class TouristDbContext:DbContext
+    {
+        public TouristDbContext(DbContextOptions options) : base(options) { 
+        
+        }
+        public DbSet<User> Users { get; set; }
+        
+    }
+}
---
+++ Bigbang3API/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Bigbang3API.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string? Username { get; set; }
+
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Role { get; set; }
+        
+        
+    }
+
+}
