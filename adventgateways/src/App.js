c204b4d620fa562c2570a10b4821be0ed7f66197
Shruthika14 <shruthika.kanini@gmail.com>
2023-08-02 01:00:23 +0530

    Initial commit of react development in Homepage

---
+++ adventgateways/src/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import Footer from "./Components/Footer";
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
+import Recommend from "./Components/Reccomend";
+import ScrollToTop from "./Components/ScrollToTop";
+import Services from "./Components/Services";
+import Testimonials from "./Components/Testimonials";
+import scrollreveal from "scrollreveal";
+export default function App() {
+  useEffect(() => {
+    const sr = scrollreveal({
+      origin: "top",
+      distance: "80px",
+      duration: 2000,
+      reset: true,
+    });
+    sr.reveal(
+      `
+        nav,
+        #hero,
+        #services,
+        #recommend,
+        #testimonials,
+        footer
+        `,
+      {
+        opacity: 0,
+        interval: 300,
+      }
+    );
+  }, []);
+  return (
+    <div>
+      <ScrollToTop />
+      <Navbar />
+      <Hero />
+      <Services />
+      <Recommend />
+      <Testimonials />
+      <Footer />
+    </div>
+  );
+}
